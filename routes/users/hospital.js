915f8e5e2310bc0b027ea630b2c8109d96937f90
ashish yoel <ashishyoel23@gmail.com>
2020-10-28 18:38:49 +0530

    selecting hospitals where image field in not null.

--- routes/users/hospital.js
+++ routes/users/hospital.js
@@ -9,8 +9,13 @@ router.get('/all/hospitals', userAuth, async (req, res) => {
     try {
         const user = await User.findOne({ _id: req.user.id }).select(['city']);
         
-        const hospitals = await Admin.find({ city: user.city })
-            .select(['-password', '-blood_data', '-date', '-email']);
+        const hospitals = await Admin.find({
+            $and: [
+                { image: { $ne: null } },
+                { city: user.city }
+            ]
+        })
+        .select(['-password', '-blood_data', '-date', '-email']);
         res.status(200).json({ hospitals }); 
     }
     catch (err) {
