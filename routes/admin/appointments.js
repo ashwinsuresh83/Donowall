45b4906701869d811f076b254bacf02262a1945f
ashish yoel <ashishyoel23@gmail.com>
2020-11-01 00:35:09 +0530

    user populated on admin get appointments api.

--- routes/admin/appointments.js
+++ routes/admin/appointments.js
@@ -3,14 +3,17 @@ const router = express.Router();
 const adminAuth = require('../../middleware/adminAuth');
 const Appointment = require('../../models/Appointment');
 
-router.get('/',adminAuth, async (req,res)=>{
-    try{
-        let appointments= await Appointment.find({admin:req.user.id})
-        res.status(200).json(appointments)
+router.get('/', adminAuth, async (req, res) => {
+    try {
+        const appointments = await Appointment
+            .find({ admin: req.user.id })
+            .populate('user', ['f_name', 'l_name']);
+        res.status(200).json(appointments);
     }
-    catch(err){
-        console.log(err)
+    catch(err) { 
+        console.log(err.message);
+        res.status(500).send('Server Error');
     }
 })
 
-module.exports=router
+module.exports = router;
